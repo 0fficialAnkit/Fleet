efa81830590d334aa6238b71590476f967c792d2
0fficialAnkit <ankitsingh2576@gmail.com>
2026-05-13 09:22:29 +0530

    Initial Commit

---
+++ Fleet/FleetApp.swift
@@ -0,0 +1,17 @@
+//
+//  FleetApp.swift
+//  Fleet
+//
+//  Created by Ankit Kumar on 13/05/26.
+//
+
+import SwiftUI
+
+@main
+struct FleetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
